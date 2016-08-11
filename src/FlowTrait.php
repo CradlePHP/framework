2e12e94c5d6f64a4fc858fe41d95bd2594d6f6c7
Christian Blanquera <cblanquera@openovate.com>
2016-08-11 21:23:27 +0800

    pulled from schema package
    generally useful

---
+++ src/FlowTrait.php
@@ -0,0 +1,91 @@
+<?php //-->
+/**
+ * This file is part of the Cradle PHP Library.
+ * (c) 2016-2018 Openovate Labs
+ *
+ * Copyright and license information can be found at LICENSE.txt
+ * distributed with this package.
+ */
+
+namespace Cradle\Framework;
+
+use Cradle\Resolver\ResolverTrait;
+
+/**
+ * Trait for controller classes in packages or custom
+ *
+ * @vendor   Cradle
+ * @package  Framework
+ * @author   Christian Blanquera <cblanquera@openovate.com>
+ * @standard PSR-2
+ */
+trait FlowTrait
+{
+    use ResolverTrait;
+
+    /**
+     * @var array $actions
+     */
+    protected $actions = array();
+
+    /**
+     * @var array $current
+     */
+    protected $current = null;
+
+    /**
+     * Returns the action property
+     * example `Flow::auth()->load()`
+     * example `Flow::auth()->search()->load()`
+     * example `Flow::auth()->search->load()`
+     *
+     * @param *string $name
+     * @param *array  $args
+     *
+     * @return string
+     */
+    public function __callController($name, $args)
+    {
+        if(is_null($this->current)) {
+            if(!isset($this->actions[$name])) {
+              return $this;
+            }
+
+            //its the search part in `Flow::auth()->search()->load()`
+            $this->current = $this->actions[$name];
+            return $this;
+        }
+
+        //its the load part in `Flow::auth()->search()->load()`
+        $action = $this->current;
+        $this->current = null;
+
+        if(property_exists($action, $name)) {
+          return $action->$name;
+        }
+
+        return function($request, $response) use ($action, $name, &$args) {
+            //we should throw a method exist error at runtime
+            $results = $action->$name($request, $response, ...$args);
+
+            if ($results instanceof $action) {
+                return;
+            }
+
+            return $results;
+        };
+    }
+
+    /**
+     * Returns the action property
+     * example `Flow::auth()->search->load`
+     *
+     * @param *string $name
+     *
+     * @return string|callable
+     */
+    public function __get($name)
+    {
+        return $this->__callController($name, array());
+    }
+}
